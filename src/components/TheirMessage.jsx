8a1339ac415d5c9fe44db50d040d1f51279b76f2
Yusin <sunkatabeckama@gmail.com>
2022-01-21 19:52:23 +0100

    Send button, image upload and handling onChange and onSubmit events

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,4 +1,34 @@
-const TheirMessage = () => {
-  return <div>TheirMessage</div>;
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByuser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByuser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByuser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#CABCDC",
+            marginLeft: isFirstMessageByuser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
 };
 export default TheirMessage;

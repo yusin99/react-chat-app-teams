bc3ce88e3c64d9579878cd67c789fed3a98a8b0e
Yusin <sunkatabeckama@gmail.com>
2022-01-21 22:02:17 +0100

    Final modifications + creating sign in form to authentificate user

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 import ChatFeed from "./components/ChatFeed";
+import Modal from "./components/LoginFrom";
 function App() {
+  if (!window.localStorage.getItem("username")) return <Modal />;
   return (
     <ChatEngine
       height="100vh"
       projectID="f247b33c-71be-41e9-b02e-c7392280d145"
-      userName="yusinsedrati"
-      userSecret="123456"
+      userName={window.localStorage.getItem("username")}
+      userSecret={window.localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
